4126f9bb32a77703f3ad02e232fbd95b91295d7d
durga15 <34118507+durga15@users.noreply.github.com>
2025-12-11 17:09:54 +0530

    Create PositiveNegativeandZero.java

---
+++ PositiveNegativeandZero.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class PositiveNegativeandZero {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the input: ");
+		int sc = input.nextInt();
+		if (sc > 0) {
+			System.out.println("The given Number " + sc + " is positive");
+		}else if(sc < 0) {
+			System.out.println("The given Number " + sc + " is negative");
+		}else {
+			System.out.println("The given Number " + sc + " is Zero");
+		}
+	}
+
+}
