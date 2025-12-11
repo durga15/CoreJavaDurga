8bcfbb65914b7730979de839776677f2fb42907a
durga15 <34118507+durga15@users.noreply.github.com>
2025-12-11 17:49:33 +0530

    Update LargestofTwoNumbers.java

--- LargestofTwoNumbers.java
+++ LargestofTwoNumbers.java
@@ -1,15 +1,12 @@
 import java.util.Scanner;
 
 public class LargestofTwoNumbers {
-
 	public static void main(String[] args) {
-		
 		 Scanner num1 = new Scanner(System.in);
 		 System.out.println("Enter First Number: ");
-		 int num3 = num1.nextInt();		
-		 Scanner num2 = new Scanner(System.in);
+		 int num3 = num1.nextInt();				
 		 System.out.println("Enter second Number: ");
-		 int num4 = num2.nextInt();		 
+		 int num4 = num1.nextInt();		 
 		 if(num3 > num4) {
 			 System.out.println("The First Number " + num3 + " is Largest");
 		 }else {
