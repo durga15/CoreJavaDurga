aca1109dc7500e459d884f35c261360eed10f051
durga15 <34118507+durga15@users.noreply.github.com>
2025-12-11 17:19:16 +0530

    Create EvenorOdd.java

---
+++ EvenorOdd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class EvenorOdd {
+
+	public static void main(String[] args) {
+	
+		Scanner ip1 = new Scanner(System.in);
+		System.out.println("Enter the Number:");
+		int ip2 = ip1.nextInt();
+		if(ip2 % 2 == 0) {
+			System.out.println("The given number " + ip2 + " is even");
+		}else {
+			System.out.println("The given number " + ip2 + " is odd");
+		}
+
+	}
+
+}
